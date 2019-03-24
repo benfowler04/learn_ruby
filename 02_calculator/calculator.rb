94c3d21eee4a165ae0891f1e5604f0ef93457554
Ben Fowler <46171044+benfowler04@users.noreply.github.com>
2019-03-24 17:42:09 -0400

    solved exercises 00-02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,36 @@
-#write your code here
+def add(a, b)
+    a + b
+end
+
+def subtract(a, b)
+    a - b
+end
+
+def sum(numbers)
+    sum = 0
+    numbers.each do |i|
+        sum += i
+    end
+    sum
+end
+
+def multiply(numbers)
+    product = 1
+    numbers.each do |i|
+        product *= i
+    end
+    product
+end
+
+def power(a, b)
+    a ** b
+end
+
+def factorial(a)
+    result = 1
+    while a > 0
+        result *= a
+        a -= 1
+    end
+    result
+end
