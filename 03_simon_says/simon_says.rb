41b5583612d05b53fb8373aee77ea6575f5ad8ca
Ben Fowler <46171044+benfowler04@users.noreply.github.com>
2019-03-24 19:02:00 -0400

    solved exercise 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,29 @@
-#write your code here
+def echo(string)
+    string
+end
+
+def shout(string)
+    string.upcase
+end
+
+def repeat(string, repeats = 2)
+    ((string + ' ') * repeats).chomp(' ')
+end
+
+def start_of_word(string, number_of_letters)
+    string[0..number_of_letters -1]
+end
+
+def first_word(string)
+    string.split[0]
+end
+
+def titleize(string)
+    words = string.capitalize.split
+    words.each do |word|
+        if word.length > 3 && word != "over"
+            word = word.capitalize!
+        end
+    end
+    words.join(' ').chomp(' ')
+end
