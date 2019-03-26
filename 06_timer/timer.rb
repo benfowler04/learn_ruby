120d6df8280aced08012d7d62095ecb062c59516
Ben Fowler <46171044+benfowler04@users.noreply.github.com>
2019-03-26 19:31:08 -0400

    solved exercise 06

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,35 @@
 class Timer
-  #write your code here
+
+    attr_reader :seconds
+  
+    def initialize
+        @seconds = 0
+        @minutes = 0
+        @hours = 0
+        @time_string = "00:00:00"
+    end
+
+    def seconds=(number)
+        @hours = number / 3600
+        number = number - @hours * 3600
+        @minutes = number / 60
+        number = number - @minutes * 60
+        @seconds = number       
+    end
+
+    def time_string
+        @hours = padded(@hours)
+        @minutes = padded(@minutes)
+        @seconds = padded(@seconds)
+        "#{@hours}:#{@minutes}:#{@seconds}"
+    end
+
+    def padded(number)
+        padded_number = number.to_s
+        if padded_number.length == 1
+            padded_number = "0" + padded_number
+        end
+        padded_number
+    end
+
 end
