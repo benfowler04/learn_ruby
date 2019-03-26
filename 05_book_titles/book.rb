f089f1a8e498f0c7df235befb9e615956f0f6365
Ben Fowler <46171044+benfowler04@users.noreply.github.com>
2019-03-26 19:09:10 -0400

    solved exercise 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,15 @@
 class Book
-# write your code here
+
+    attr_reader :title
+
+    def title=(string)
+        skip_words = ['the', 'a', 'an', 'and', 'in', 'of']
+        @title = string.split.each_with_index do |word, index|
+            if !skip_words.include?(word) || index == 0
+                word = word.capitalize!
+            end
+        end
+        @title = @title.join(' ')
+    end
+
 end
